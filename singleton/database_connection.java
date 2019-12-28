019f3fccdc98f73da3a8876f7053f2a7b98ad12e
Mahmoud Ahmed <mahmoud_ahmed@stud.fci-cu.edu.eg>
2019-12-28 22:00:13 +0200

    Create database_connection.java

---
+++ singleton/database_connection.java
@@ -0,0 +1,109 @@
+package utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DBUtil 
+{
+    public static DBUtil instance;
+    
+    // Database URL
+    public final String DB_URL = "jdbc:mysql://localhost:3306/graph_api?useSSL=false";
+
+    // Database credentials
+    public final String USERNAME = "root";
+    public final String PASSWORD = "";
+    
+    // Connection is the session between java application and database
+    private Connection connection;
+    
+    // The Statement interface provides methods to execute queries with the database
+    public Statement statement;
+    
+    // The query string value to the database
+    public String query;
+    
+    // The object of ResultSet maintains a cursor pointing to a row of a table
+    public ResultSet resultSet;
+    
+    private DBUtil() throws SQLException 
+    {
+        try
+        {
+            // Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");  
+
+            // Open a connection
+            this.connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);  
+        }
+        catch(Exception _ex)
+        {
+            System.out.println(_ex.getMessage());
+        }
+        
+    }
+    
+    public Connection getConnection() 
+    {
+        return this.connection;
+    }
+    
+    public static DBUtil getInstance() throws SQLException 
+    {
+        if(instance == null)
+        {
+            instance = new DBUtil();
+        }
+        else if(instance.getConnection().isClosed())
+        {
+            instance = new DBUtil();
+        }
+        
+        return instance;
+    }
+    
+    public ResultSet selectData(String _query) throws Exception
+    {
+        this.query = _query;
+        
+        // Creating statement
+        this.statement = this.connection.createStatement();
+        
+        // Execute query
+        this.resultSet = this.statement.executeQuery(_query);
+        
+        return this.resultSet;
+    }
+    
+    public void executeCommand(String _query) throws Exception
+    {
+        this.query = _query;
+        
+        // Creating statement
+        this.statement = this.connection.createStatement();
+        
+        // Execute query
+        this.statement.executeUpdate(_query);
+    }
+    
+    @Override
+    public void finalize() throws Throwable
+    {
+        try 
+        {
+            // Clean-up environment
+            this.resultSet.close();
+            this.statement.close();
+            this.connection.close();
+        } 
+        finally 
+        {
+            super.finalize();
+        }
+    }
+    
+}
